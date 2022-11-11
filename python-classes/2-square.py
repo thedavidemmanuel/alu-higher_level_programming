92e58ed076af78b240a4bcbff958c147e1a14880
David Emmanuel <d.emmanuel@alustudent.com>
2022-11-11 13:11:18 -0800

    Task 2 update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
- """ A class that defines a square by its size
-    """
+"""
+A class 'Square' that defines a square by: (based on 1-square.py)
+Private instance attribute: size
+Instantiation with optional size: def __init__(self, size=0):
+size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message size must be >= 0
+"""
+
 
 class Square:
-   
+    """Creates a square.
+    Private instance attributes: size
+    """
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
+        """Initializes data."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.__size =

e405bad375f248c8393c3c3ccacb2aa1bee43a95
David Emmanuel <d.emmanuel@alustudent.com>
2022-11-11 13:06:01 -0800

    Task 1 update

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""An empty class 'Square' that defines a square:"""
+"""A class Square that defines a square by: (based on 0-square.py)
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
+"""
 
 
 class Square:

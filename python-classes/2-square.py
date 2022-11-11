5eadc647c5dbd9afa25b13497f20018f119d8c2d
David Emmanuel <d.emmanuel@alustudent.com>
2022-11-11 12:56:01 -0800

    Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-""" A class that defines a square by its size
+ """ A class that defines a square by its size
     """
-    
+
 class Square:
-    
-    
+   
     def __init__(self, size=0):
         """ Method to initialize the square object
         """
-        
-        
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
-
-    def area(self):
-        """ Method that returns the square are of the object
-        """
-        return (self.__size ** 2)
+            self.__size = int(size)

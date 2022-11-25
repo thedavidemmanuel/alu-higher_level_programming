f2fdc2da52865e1c9ee0bcc187cf1edd7792e0b7
David Emmanuel <d.emmanuel@alustudent.com>
2022-11-25 09:57:34 -0800

    chmod

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a Python script that fetches https://alu-intranet.hbtn.io/status"""
+"""Python script that fetches https://alu-intranet.hbtn.io/status"""
 
 
 import urllib.request

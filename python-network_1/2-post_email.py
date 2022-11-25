52b8585f9d0a00de5dfedd42837524c51a1797e0
David Emmanuel <d.emmanuel@alustudent.com>
2022-11-25 10:15:33 -0800

    Python Network

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    """"Documented"""
+    url = sys.argv[1]
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')  # data should be bytes
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("{}".format(content.decode("utf-8")))

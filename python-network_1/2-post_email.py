b42938b2064997c7591d358d531287430aa52c2b
David Emmanuel <d.emmanuel@alustudent.com>
2022-11-25 11:19:21 -0800

    Update

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
- Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
-"""
+"""documented"""
 
 import urllib.request
 import urllib.parse
--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)."""
+"""A python script """
 
 import urllib.request
 import urllib.error

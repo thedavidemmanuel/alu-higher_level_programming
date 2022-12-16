ac56059f6b8752a43ecf7cf7e2ce3b38f5b7c863
David Emmanuel <d.emmanuel@alustudent.com>
2022-12-16 03:17:30 -0800

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,9 +13,7 @@ Python function that adds two numbers
 
 
 """
-
 def add_integer(a, b=98):
-
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
